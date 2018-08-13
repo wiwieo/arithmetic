f39ef8f46840099c2188b07d0d8f370ac2bfe4e6
wuqingwei <wu.qingwei@yujianjia.com>
2018-08-13 18:36:17 +0800

    堆选择排序

---
+++ arithmetic/sort/heap_select/heap_select_sort.go
@@ -0,0 +1,23 @@
+package sort
+
+// 堆排序
+func HeapSelectSort(ary []int){
+	for i := 0; i < len(ary); i ++{
+		adjustMaxHeap(ary, len(ary) - i)
+		ary[0], ary[len(ary) - 1 - i] = ary[len(ary) - 1 - i], ary[0]
+	}
+}
+
+// 数组本身即是被打乱的堆，需要进行调整
+// 叶子个数为：n/2 + 1
+// 第一个非叶子序号为：n/2 - 1
+func adjustMaxHeap(ary []int, last int){
+	for i := last / 2 - 1; i >= 0 ; i --{
+		if 2 * i + 1 < last && ary[2 * i + 1] > ary[i]{
+			ary[2 * i + 1], ary[i] = ary[i], ary[2 * i + 1]
+		}
+		if 2 * i + 2 < last && ary[2 * i + 2] > ary[i]{
+			ary[2 * i + 2], ary[i] = ary[i], ary[2 * i + 2]
+		}
+	}
+}
